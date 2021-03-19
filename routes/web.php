3ff6ea452fbf66a7169b6d9e208423fddb4598d2
Joseph Opio <josephopio11@gmail.com>
2021-03-19 11:53:40 +0200

    4

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\ProductsController;
-use Illuminate\Routing\Route as RoutingRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +12,7 @@ use Illuminate\Routing\Route as RoutingRoute;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // New to Laravel 8
 Route::get('/products', [ProductsController::class, 'index']);
@@ -22,4 +21,7 @@ Route::get('/products', [ProductsController::class, 'index']);
 // Route::get('/products/{id}', [ProductsController::class, 'show'])->where('id', '[0-9]+');
 
 // Pattern is a string
-Route::get('/products/{id}', [ProductsController::class, 'show'])->where('id', '[a-z]+');
+Route::get('/products/{name}/{id}', [ProductsController::class, 'show'])->where([
+    'name' => '[a-z]+',
+    'id'   => '[0-9]+',
+]);
